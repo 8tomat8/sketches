1a054bbde1b8f3fc49c6f4e9094941ec3fadbf31
Deployer <deployer@nxchange.com>
2019-05-06 10:10:12 +0200

    Added TLS support to fileServer

--- fileServer/main.go
+++ fileServer/main.go
@@ -16,12 +16,22 @@ func main() {
 		log.Fatal(errors.Wrap(err, "failed to get current file path"))
 	}
 
-	port := flag.String("p", "8100", "port to serve on")
-	directory := flag.String("d", curPath, "the directory of static file to host")
+	// Base
+	port := flag.String("port", "8100", "port to serve on")
+	directory := flag.String("dir", curPath, "the directory of static file to host")
+	// TLS
+	certPath := flag.String("cert", "", "path to the tls certificate")
+	keyPath := flag.String("key", "", "path to the tls key")
+
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
-	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	if *certPath != "" && *keyPath != "" {
+		log.Printf("Serving %s on HTTPS port: %s\n", *directory, *port)
+		log.Fatal(http.ListenAndServeTLS(":"+*port, *certPath, *keyPath, nil))
+	} else {
+		log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+		log.Fatal(http.ListenAndServe(":"+*port, nil))
+	}
 }
