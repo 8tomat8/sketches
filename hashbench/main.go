52d8595d5b248281799366c84e0f9ff4e27beb9c
Borys Hulii <BorysHulii@pvh.com>
2019-11-01 17:06:59 +0100

    Added benchmark for Blake2b

--- hashbench/main.go
+++ hashbench/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/hex"
+	"github.com/minio/blake2b-simd"
 
 	"crypto/md5"
 
@@ -16,6 +17,12 @@ func MD5(data []byte) string {
 
 func Murmur(data []byte) string {
 	h := murmur3.New128()
+	h.Write(data)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func MinioBlake2b(data []byte) string {
+	h := blake2b.New256()
 	h.Write(data)
 	return hex.EncodeToString(h.Sum(nil))
 }
--- hashbench/main_test.go
+++ hashbench/main_test.go
@@ -14,7 +14,7 @@ func mkinput(n int, inputSize int) [][]byte {
 	rv := make([][]byte, n)
 	for i := 0; i < n; i++ {
 		for j := 0; j < inputSize; j++ {
-			rv[i] = append(rv[i], uuid.Must(uuid.NewV4()).Bytes()...)
+			rv[i] = append(rv[i], uuid.NewV4().Bytes()...)
 		}
 	}
 	return rv
@@ -48,6 +48,20 @@ func benchmarkMurmur(b *testing.B, inputSize int) {
 	result = output
 }
 
+func benchmarkMinioBlake2b(b *testing.B, inputSize int) {
+	//input := mkinput(b.N, inputSize)
+	input, err := ioutil.ReadFile("./milky_way_starry_sky_galaxy_119519_3840x2160.jpg")
+	if err != nil {
+		b.Fatal(err)
+	}
+	output := make([]string, b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		output[i] = Murmur(input)
+	}
+	result = output
+}
+
 func BenchmarkMD5_16(b *testing.B)   { benchmarkMD5(b, 1) }
 func BenchmarkMD5_160(b *testing.B)  { benchmarkMD5(b, 10) }
 func BenchmarkMD5_1600(b *testing.B) { benchmarkMD5(b, 100) }
@@ -57,5 +71,11 @@ func BenchmarkMD5_1600(b *testing.B) { benchmarkMD5(b, 100) }
 func BenchmarkMurmur_16(b *testing.B)   { benchmarkMurmur(b, 1) }
 func BenchmarkMurmur_160(b *testing.B)  { benchmarkMurmur(b, 10) }
 func BenchmarkMurmur_1600(b *testing.B) { benchmarkMurmur(b, 100) }
+
+//func BenchmarkMurmur_16000(b *testing.B) { benchmarkMurmur(b, 1000) }
+
+func BenchmarkMinioBlake2b_16(b *testing.B)   { benchmarkMinioBlake2b(b, 1) }
+func BenchmarkMinioBlake2b_160(b *testing.B)  { benchmarkMinioBlake2b(b, 10) }
+func BenchmarkMinioBlake2b_1600(b *testing.B) { benchmarkMinioBlake2b(b, 100) }
 
 //func BenchmarkMurmur_16000(b *testing.B) { benchmarkMurmur(b, 1000) }
